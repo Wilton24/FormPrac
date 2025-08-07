11c27461d7e6d0bbd92ff31030449b08747623a1
Wilton Batiquin <cbasher24@gmail.com>
2025-08-07 19:30:46 +0800

    Refactor Login component to use consolidated state management and add Signup component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,28 +1,24 @@
 import React, { useState } from "react";
 
 export default function Login() {
+  const [credentials, setCredentials] = useState<{ email: string | null; password: string | null }>({ email: null, password: null });
 
-  const [email, setEmail] = useState<string | null>('');
-  const [password, setPassword] = useState<string | null>('');
-
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    console.log('Login form submitted');
-  };
-
-  function onEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setEmail(event.target.value);
-  };
-
-  function onPasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setPassword(event.target.value);
+  function onInputChange(identifier: string, value: React.ChangeEvent<HTMLInputElement>) {
+    setCredentials(prevState => ({
+      ...prevState,
+      [identifier]: value
+    }));
   };
 
   function handleClearForm(event: React.MouseEvent<HTMLButtonElement>) {
-    setEmail("");
     event.preventDefault();
-    setPassword("");
+    setCredentials({ email: null, password: null });
     console.log('Form cleared');
+  };
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('Form submitted with:', credentials);
   };
 
   return (
@@ -32,12 +28,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input onChange={onEmailChange} id="email" type="email" name="email" value={email || ''} />
+          <input onChange={(e: any) => onInputChange('email', e.target.value)} id="email" type="email" name="email" value={credentials.email || ''} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input onChange={onPasswordChange} id="password" type="password" name="password" value={password || ''} />
+          <input onChange={(e: any) => onInputChange('password', e.target.value)} id="password" type="password" name="password" value={credentials.password || ''} />
         </div>
       </div>
 
